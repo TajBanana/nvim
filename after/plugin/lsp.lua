5cc7a53a1f5264d8576facb0a83e2cd401961ce7
TajBanana <ytahjian@gmail.com>
2023-12-30 22:35:34 +0800

    update keymap

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -47,15 +47,15 @@ lsp_zero.set_preferences({
 lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "<leader>def", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "]e", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "[e", function() vim.diagnostic.goto_prev() end, opts)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "<leader>ref", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>rf", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
