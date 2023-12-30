483db91d8c0132e992a6e267520543b029a8dc5e
TajBanana <ytahjian@gmail.com>
2023-12-30 17:06:51 +0800

    format param

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,5 +1,21 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
-	ensure_installed = { "lua_ls", "jdtls", "tsserver", "eslint", "jsonls", "tailwindcss", "yamlls", "sqlls", "kotlin_language_server", "gradle_ls", "dockerls", "cssls", "emmet_ls", "graphql","html"},
+	ensure_installed = {
+	"lua_ls",
+	"jdtls",
+	"tsserver",
+	"eslint",
+	"jsonls",
+	"tailwindcss",
+	"yamlls",
+	"sqlls",
+	"kotlin_language_server",
+	"gradle_ls",
+	"dockerls",
+	"cssls",
+	"emmet_ls",
+	"graphql",
+	"html"
+	},
 }
 
