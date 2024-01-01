faa2018d6859910f4796f6b410f5895c1b999ded
TajBanana <ytahjian@gmail.com>
2024-01-02 01:19:30 +0800

    add ability to search for hidden files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,25 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fw', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ")});
 end)
+
+local telescope = require('telescope')
+
+telescope.setup {
+    pickers = {
+        find_files = {
+            hidden = true
+        }
+    }
+}
+
+require("telescope").setup {
+    defaults = {
+        file_ignore_patterns = {
+            ".git/",
+            ".idea/",
+            ".vscode/",
+            ".gitlab/"
+        }
+    }
+}
+
